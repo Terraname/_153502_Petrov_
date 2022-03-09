2285b0571240eca13f631aeaf2f27ef937bc0f39
andrewhalf55@gmail.com <andrewhalf55@gmail.com>
2022-03-10 02:41:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _153502_Petrov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter 1st number ");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number ");
+            b = Convert.ToInt16(Console.ReadLine());
+            c = a / b;
+            Console.WriteLine($"The quotient is {c}");
+        }
+    }
+}
