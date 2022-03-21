76f70d7fc9f428ce4a03068b50609bda759e36fd
Terraname <91671919+Terraname@users.noreply.github.com>
2022-03-22 00:27:01 +0300

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Petrov_lab2_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Enter x");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            y = Convert.ToDouble(Console.ReadLine());
+            double dist = x * x + y * y;
+            if (dist > 15 * 15 && dist < 25 * 25)
+            {
+                Console.WriteLine("Да");
+            }
+            else if (dist == 15 * 15 || dist == 25 * 25)
+            {
+                Console.WriteLine("На границе");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }         
+        }
+
+        static int check(double x, double y)
+        {
+            double dist = x * x + y * y;
+            if (dist > 15 * 15 && dist < 25 * 25)
+            {
+                return 1;
+            }
+            else if (dist == 15 * 15 || dist == 25 * 25)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ lab2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() => Assert.AreEqual(1, Petrov_)
+        {
+            Assert.AreEqual(1, Petrov_lab2_task2::)
+        }
+    }
+}
